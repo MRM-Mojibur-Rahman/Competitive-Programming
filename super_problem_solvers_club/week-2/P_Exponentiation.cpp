e8f7abeccc847c8bafeebed817f841bf73343b9a
MRM-Mojibur-Rahman <mojibur.rahman102006@gmail.com>
2026-02-02 21:24:39 +0600

    Binary Exponential

---
+++ super_problem_solvers_club/week-2/P_Exponentiation.cpp
@@ -0,0 +1,37 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+const ll mod = 1000000007;
+
+void solve()
+{
+    ll a, b;
+    cin >> a >> b;
+    ll ans = 1;
+    ll it = a;
+    while (b)
+    {
+        if (b & 1)
+        {
+            ans = (ans%mod * (a%mod))%mod;
+        }
+        
+        b >>= 1;
+        a = 1LL * a * a % mod;
+    }
+    cout<<ans<<endl;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
