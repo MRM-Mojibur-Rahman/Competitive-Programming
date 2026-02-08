df5171329805f03740789a8b698b0679554b4177
MRM-Mojibur-Rahman <mojibur.rahman102006@gmail.com>
2026-02-08 10:04:03 +0600

    B T prime is soved

--- super_problem_solvers_club/week-2/B_T_primes.cpp
+++ super_problem_solvers_club/week-2/B_T_primes.cpp
@@ -2,38 +2,36 @@
 using namespace std;
 #define ll long long
 
+bool isPrime(ll x)
+{
+    if(x==1 ) return false;
+    for (int i = 2; i*i<= x; i++)
+    {
+        if(x%i==0) return false;
+    }
+    return true;
+    
+}
+bool isSqrt(ll x)
+{
+    ll y = sqrtl(x);
+    return x==y*y;
+}
 int main()
 {
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    ll n;
-    cin >> n;
-    vector<ll> a(n);
-    for (auto &it : a)
-        cin >> it;
-    for (ll i = 0; i < n; i++)
-    {
-        map<ll, ll> prime_fact;
-        for (ll j = 2; j * j <= a[i]; j++)
-        {
-            while (a[i] % j == 0)
-            {
-                prime_fact[j]++;
-                a[i] /= j;
-            }
-        }
-        if (a[i] > 1)
-            prime_fact[a[i]]++;
-        ll div = 1;
-        for (auto [x, y] : prime_fact)
-        {
-            div *= (y + 1);
-        }
-        if (div == 3)
-            cout << "YES\n";
-        else
-            cout << "NO\n";
-    }
 
+    ll n;cin>>n;
+    for (int i = 0; i < n; i++)
+    {
+        ll x;cin>>x;
+        if(isSqrt(x)&&isPrime(sqrtl(x)))
+        {
+            cout<<"YES\n";
+        }
+        else cout<<"NO\n";
+    }
+    
     return 0;
 }
