fac16598f6f6cc996f1dbcd7685e92751612981b
MRM-Mojibur-Rahman <mojibur.rahman102006@gmail.com>
2026-03-02 11:50:07 +0600

    C Queen is sovled

--- super_problem_solvers_club/week-5(Tree and Graph) /C_Queen.cpp
+++ super_problem_solvers_club/week-5(Tree and Graph) /C_Queen.cpp
@@ -7,7 +7,30 @@ int main()
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    
-    
+    int n;
+    cin >> n;
+    vector<int> adj[n + 1], bd(n + 1, 0), a(n + 1, 0);
+    for (int i = 1; i <= n; i++)
+    {
+        int p, c;
+        cin >> p >> c;
+        if(p!=-1)
+        adj[p].push_back(i);
+        if (c == 1)
+        {
+            a[i] = 1;
+            bd[p]++;
+        }
+    }
+    vector<int> ans;
+    for (int i = 1; i <= n; i++)
+    {
+        if ((int)adj[i].size() == bd[i] && a[i] == 1)
+            ans.push_back(i);
+    }
+    if(ans.size()!=0)
+    for(auto it:ans) cout<<it<<" ";
+    else cout<<-1<<endl;
+
     return 0;
 }
