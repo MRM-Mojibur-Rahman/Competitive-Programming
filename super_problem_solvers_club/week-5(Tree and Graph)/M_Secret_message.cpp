33aff816797c9ae3256084f512eafaa5aded44ac
MRM-Mojibur-Rahman <mojibur.rahman102006@gmail.com>
2026-03-04 13:12:10 +0600

    M secret message is solved

--- super_problem_solvers_club/week-5(Tree and Graph)/M_Secret_message.cpp
+++ super_problem_solvers_club/week-5(Tree and Graph)/M_Secret_message.cpp
@@ -4,30 +4,51 @@ using namespace std;
 
 void solve()
 {
-    int n, k;cin>>n>>k;
-    vector<int>a(n,0);
+    int n, k;
+    cin >> n >> k;
+    vector<int> a(n, 0);
     for (int i = 0; i < k; i++)
     {
-        string x;cin>>x;
+        string x;
+        cin >> x;
         for (int j = 0; j < n; j++)
         {
-            a[j]|=(1<<(x[i]-'a'));
+            a[j] |= (1 << (x[j] - 'a'));
         }
-        
     }
-    set<int>div;
-    for (int i = 1; i*i <= n; i++)
+    set<int> div;
+    for (int i = 1; i * i <= n; i++)
     {
-        if(n%i==0)
+        if (n % i == 0)
         {
-            div.insert(n/i);
+            div.insert(n / i);
             div.insert(i);
         }
+    }
+
+    for (int d : div)
+    {
+        string ans ;
+        bool exist = true;
+       
+        for (int i = 0; i < d; i++)
+        {
+             int alphabet = (1<<26)-1;
+            for (int j = i; j < n; j+=d) alphabet&=a[j];
+            if(!alphabet) {exist = false; break;}
+            ans+=(char)('a'+ __builtin_ctz(alphabet));
+        }
+        if(exist)
+        {
+            for(int i=0;i<n/d;i++)
+            {
+                cout<<ans;
+            }
+            cout<<endl;
+            return;
+        }
         
     }
-    
-    
-    
 }
 
 int main()
