fa91645defdcc581f05a4c9928cb39566b0b5a52
MRM-Mojibur-Rahman <mojibur.rahman102006@gmail.com>
2026-03-09 22:11:50 +0600

    Removing Digits is solved

--- super_problem_solvers_club/week-6(DP)/Removing_Digits.cpp
+++ super_problem_solvers_club/week-6(DP)/Removing_Digits.cpp
@@ -1,23 +1,24 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define ll long long
-const int mxn = 1e6+7;
-vector<int>dp(mxn,-1);
+const int mxn = 1e6 + 7;
+vector<int> dp(mxn, -1);
 int rv(int n)
 {
-    if(n==0) return 0;
-    if(dp[n]!=-1) return dp[n];
-    int tem= n;
+    if (n == 0)
+        return 0;
+    if (dp[n] != -1)
+        return dp[n];
+    int tem = n;
     int ans = INT_MAX;
-    while (tem!=0)
+    while (tem != 0)
     {
-        int rem=(tem%10);
-        if(rem!=0)
-        ans = min(ans,rv(n-rem)+1);
-        tem/=10;
+        int rem = (tem % 10);
+        if (rem != 0)
+            ans = min(ans, rv(n - rem) + 1);
+        tem /= 10;
     }
-    return dp[n]=ans;
-    
+    return dp[n] = ans;
 }
 
 int main()
@@ -27,9 +28,28 @@ int main()
 
     int n;
     cin >> n;
-    
-    cout<<rv(n)<<endl;
+    vector<int> dp(n + 1, 0);
+    for (int i = 0; i <= 9; i++)
+    {
+        dp[i] = 1;
+    }
+    for (int i = 10; i <= n; i++)
+    {
+        int tem = i;
+        int ans = INT_MAX;
 
+        while (tem != 0)
+        {
+            int rem = (tem % 10);
+            if (rem != 0)
+                ans = min(ans, dp[i - rem] + 1);
+            tem /= 10;
+        }
+        dp[i]= ans;
+    }
+    cout<<dp[n]<<endl;
+
+    // cout<<rv(n)<<endl;
 
     return 0;
 }
