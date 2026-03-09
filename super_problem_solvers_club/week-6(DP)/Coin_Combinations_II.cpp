b89180f517e72aa28fb88be722b73e4aaf15541d
MRM-Mojibur-Rahman <mojibur.rahman102006@gmail.com>
2026-03-09 19:34:40 +0600

    Coin Combinations is solved

--- super_problem_solvers_club/week-6(DP)/Coin_Combinations_II.cpp
+++ super_problem_solvers_club/week-6(DP)/Coin_Combinations_II.cpp
@@ -7,34 +7,46 @@ int main()
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    int n, x;cin>>n>>x;
-    vector<int>a(n);
+    int n, x;
+    cin >> n >> x;
+    vector<int> a(n);
     for (int i = 0; i < n; i++)
     {
-       cin>>a[i];
+        cin >> a[i];
     }
-    sort(a.begin(),a.end());
+    sort(a.begin(), a.end());
+    // vector<vector<ll>> dp(n+1, vector<ll>(x + 1));
+    // vector<int>nextState(x+1,0);
+    // nextState[0]=1;
 
-   vector<vector<ll>>dp(n,vector<ll>(x+1));
-   for (int i = 0; i < n; i++)
-   {
-    dp[i][0]=1;
-   }
-   for (int i = n-1; i >=0; i--)
-   {
-    for (int sum= 0; sum<=x; sum++)
+    // // for (int i = 0; i < n; i++)
+    // // {
+    // //     dp[i][0] = 1;
+    // // }
+    // for (int i = n - 1; i >= 0; i--)
+    // {
+    // vector<int>currentState(x+1,0);
+    // currentState[0]=1;
+
+    //     for (int sum = 1; sum <= x; sum++)
+    //     {
+    //         ll skip = nextState[sum];
+    //         ll take = ((a[i] <= sum) ? currentState[sum - a[i]] : 0);
+    //         currentState[sum] = (take + skip)%mod;
+    //     }
+    //     nextState=currentState;
+    // }
+
+    // cout << nextState[x] << endl;
+    vector<ll> dp(x + 1);
+    dp[0] = 1;
+
+    for (int coin : a)
     {
-        /* code */
+        for (int sum = coin; sum <= x; sum++)
+            dp[sum] = (dp[sum] + dp[sum - coin]) % mod;
     }
-    
-   }
-   
-   
+    cout << dp[x] << endl;
 
-   
-   cout<<dp[x]<<endl;
-   
-    
-    
     return 0;
 }
