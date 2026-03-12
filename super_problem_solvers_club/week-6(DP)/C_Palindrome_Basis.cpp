9f96bd2627e87ab7f7ef8ae302dac93a9f6c0ad3
MRM-Mojibur-Rahman <mojibur.rahman102006@gmail.com>
2026-03-12 06:42:57 +0600

    book shop  is solved

--- super_problem_solvers_club/week-6(DP)/C_Palindrome_Basis.cpp
+++ super_problem_solvers_club/week-6(DP)/C_Palindrome_Basis.cpp
@@ -1,76 +1,36 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define ll long long
-const ll mod = 1e9 + 7;
-const int mxn = 5e4 + 2;
-
-// vector<int> palindromes;
-// vector<int> dp(mxn, -1);
-
-// void palindrome(int n)
-// {
-//     int x = n;
-//     vector<int> tem, dig;
-//     while (n != 0)
-//     {
-//         tem.push_back(n % 10);
-//         n /= 10;
-//     }
-//     dig = tem;
-//     reverse(tem.begin(), tem.end());
-//     if (tem == dig)
-//         palindromes.push_back(x);
-// }
-// ll ans(int i)
-// {
-//     if (i == 0)
-//         return 1;
-
-//     if (dp[i] != -1)
-//         return dp[i];
-//     ll it = 0;
-//     int len = palindromes.size();
-//     for (int j = 0; j < len; j++)
-//     {
-//         if(palindromes[j]>i) break;
-//         it += (i - palindromes[j] >= 0) ? ans(i - palindromes[j]) : 0;
-//     }
-//     return dp[i] = it % mod;
-// }
-// void solve()
-// {
-//     int n;
-//     cin >> n;
-//     cout<<ans(n)<<endl;
-// }
-
-// int main()
-// {
-//     ios::sync_with_stdio(false);
-//     cin.tie(nullptr);
-
-//     int t;
-//     cin >> t;
-//     for (int i = 1; i < mxn; i++)
-//     {
-//         palindrome(i);
-//     }
-
-//     while (t--)
-//     {
-//         // for(int i = 0 ; i< mxn; i++) dp[i]=-1;
-//         solve();
-//     }
-//     return 0;
-// }
-vector<int> pal;
-
-bool isPal(int x)
+const int mod = 1e9 + 7;
+vector<int> palindromes;
+void check_palindromes(int i)
 {
-    string s = to_string(x);
-    string t = s;
-    reverse(t.begin(), t.end());
-    return s == t;
+    string a = to_string(i);
+    string tem = a;
+    reverse(tem.begin(), tem.end());
+    if (tem == a)
+        palindromes.push_back(i);
+}
+vector<ll> dp(4e4 + 1, 0);
+void ans()
+{
+    int len = palindromes.size();
+    dp[0] = 1;
+    for (int i = 0; i < len; i++)
+    {
+        for (int sum = palindromes[i]; sum <= 4e4 ; sum++)
+        {
+            dp[sum] = (dp[sum] + dp[sum - palindromes[i]]) % mod;
+        }
+    }
+}
+
+void solve()
+{
+    int n;
+    cin >> n;
+
+    cout << dp[n] << endl;
 }
 
 int main()
@@ -78,22 +38,14 @@ int main()
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    for(int i=1;i<mxn;i++)
-        if(isPal(i)) pal.push_back(i);
-
-    vector<ll> dp(mxn);
-    dp[0] = 1;
-
-    for(auto p:pal)
-        for(int i=p;i<mxn;i++)
-            dp[i] = (dp[i] + dp[i-p]) % mod;
-
-    int t; 
-    cin>>t;
-    while(t--)
+    int t;
+    cin >> t;
+    for (int i = 1; i <= 4e4; i++)
+        check_palindromes(i);
+    ans();
+    while (t--)
     {
-        int n;
-        cin>>n;
-        cout<<dp[n]<<"\n";
+        solve();
     }
+    return 0;
 }
