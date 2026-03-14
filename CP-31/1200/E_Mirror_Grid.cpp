5ee9a20a850052ac5132a3b6b5ea61621cb50ea8
MRM-Mojibur-Rahman <mojibur.rahman102006@gmail.com>
2026-03-14 13:08:24 +0600

    Mirror grid   -1200  is solved

--- CP-31/1200/E_Mirror_Grid.cpp
+++ CP-31/1200/E_Mirror_Grid.cpp
@@ -4,7 +4,29 @@ using namespace std;
 
 void solve()
 {
-    
+    int n;
+    cin >> n;
+
+    vector<vector<int>> a(n + 1, vector<int>(n + 1));
+    for (int i = 0; i < n; i++)
+
+        for (int j = 0; j < n; j++)
+        {
+            char x;
+            cin >> x;
+            a[i][j] = x - '0';
+        }
+
+    int cnt = 0;
+    for (int i = 0; i < (n+1)/2; i++)
+    {
+        for (int j = 0; j <n/2; j++)
+        {
+            int s = a[i][j] + a[j][n-i-1] + a[n-i-1][n - j - 1] + a[n - j- 1][i];
+            cnt += min(4 - s, s);
+        }
+    }
+    cout << cnt << endl;
 }
 
 int main()
