99e803c20ce607fdf1b5a596dcc532c924cdb114
MRM-Mojibur-Rahman <mojibur.rahman102006@gmail.com>
2026-03-16 06:22:04 +0600

    And 0 sum big -1200  is solved

---
+++ CP-31/1200/B_AND_0_Sum_Big.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+const int mod = 1e9 + 7;
+void solve()
+{
+    ll n, k;
+    cin >> n >> k;
+    ll ans = 1;
+    for (int i = 0; i < k; i++)
+    {
+        ans = ((ans % mod) * (n % mod)) % mod;
+    }
+    cout<<ans<<endl;
+}
+
+int main()
+{
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
